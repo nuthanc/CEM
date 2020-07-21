e79d49c3d4e92dcd35c76e3b66be4937c93d92ea
nuthanc <nuthanc@juniper.net>
2020-07-21 10:24:51 -0700

    Fix while condition

--- 13066/status.sh
+++ 13066/status.sh
@@ -4,7 +4,7 @@ i34_status=$(juju status|grep nodei34)
 c9_status=$(juju status|grep nodec9)
 c61_status=$(juju status|grep nodec61)
 
-while [[ ${n20_status} != *"Deployed"* ]] && [[ ${n19_status} != *"Deployed"* ]] && [[ ${i34_status} != *"Deployed"* ]] && [[ ${c9_status} != *"Deployed"* ]] && [[ ${c61_status} != *"Deployed"* ]]; do
+while [[ ${n20_status} != *"Deployed"* ]] || [[ ${n19_status} != *"Deployed"* ]] || [[ ${i34_status} != *"Deployed"* ]] || [[ ${c9_status} != *"Deployed"* ]] || [[ ${c61_status} != *"Deployed"* ]]; do
 	echo "Deploying"
 	n20_status=$(juju status|grep noden20)
 	n19_status=$(juju status|grep noden19)

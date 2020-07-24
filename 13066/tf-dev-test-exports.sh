de345df6649874c1e292bad690a45115a2390ab3
nuthanc <nuthanc@juniper.net>
2020-07-24 01:55:35 -0700

    Add tf-dev-test exports

---
+++ 13066/tf-dev-test-exports.sh
@@ -0,0 +1,14 @@
+export ORCHESTRATOR=all
+export CONTROLLER_NODES=192.168.30.20
+export AGENT_NODES="192.168.30.9,192.168.30.34"
+export CONTAINER_REGISTRY=opencontrailnightly
+export CONTRAIL_CONTAINER_TAG=2008.20
+export SSL_ENABLE=True
+export SSL_ENABLE=false
+export DEPLOYER=juju
+export OPENSTACK_VERSION=queens
+export AUTH_PASSWORD=password
+export AUTH_DOMAIN=admin_domain
+export AUTH_REGION=RegionOne
+export AUTH_URL="http://192.168.30.120:35357/v3"
+export AUTH_PORT=35357

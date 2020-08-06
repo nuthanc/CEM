d40580091ee9a3a5c4e7c7ab0becfc99296d8340
nuthanc <nuthanc@juniper.net>
2020-08-05 23:53:30 -0700

    Update status.sh

--- 13066/status.sh
+++ 13066/status.sh
@@ -1,19 +1,19 @@
 n20_status=$(juju status|grep noden20)
-n19_status=$(juju status|grep noden19)
+n29_status=$(juju status|grep noden29)
 #i34_status=$(juju status|grep nodei34) need to add in while condition also
-c9_status=$(juju status|grep nodec9)
+i34_status=$(juju status|grep nodei34)
 # c61_status=$(juju status|grep nodec61)
 
-while [[ ${n20_status} != *"Deployed"* ]] || [[ ${n19_status} != *"Deployed"* ]] || [[ ${c9_status} != *"Deployed"* ]]; do
+while [[ ${n20_status} != *"Deployed"* ]] || [[ ${n29_status} != *"Deployed"* ]] || [[ ${i34_status} != *"Deployed"* ]]; do
 	echo "Deploying"
 	n20_status=$(juju status|grep noden20)
-	n19_status=$(juju status|grep noden19)
+	n29_status=$(juju status|grep noden29)
 	#i34_status=$(juju status|grep nodei34)
-	c9_status=$(juju status|grep nodec9)
+	i34_status=$(juju status|grep nodei34)
 	# c61_status=$(juju status|grep nodec61)
 	echo $n20_status
-	echo $n19_status
-	echo $c9_status
+	echo $n29_status
+	echo $i34_status
 	# echo $c61_status
 	sleep 5
 done

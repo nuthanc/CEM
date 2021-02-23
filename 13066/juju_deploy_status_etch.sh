025dd7a40ed3cfbed18ad6efe690adc17c415ff7
nuthanc <nuthanc@juniper.net>
2021-02-23 03:25:22 -0800

    Update juju_deploy

--- 13066/juju_deploy_status_etch.sh
+++ 13066/juju_deploy_status_etch.sh
@@ -1,1 +1,1 @@
-juju deploy ./my-k8s-os-mi.yaml && bash status.sh && python3 /root/juju_deployment/scripts/etc-hosts.py
+juju deploy ./my-k8s-os-mi.yaml && bash status.sh && python3 /root/juju_deployment/scripts/etc-hosts.py && python3 /root/juju_deployment/scripts/etc-hosts-in-template.py

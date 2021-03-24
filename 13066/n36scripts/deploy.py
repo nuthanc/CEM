e4640cdcf905132782c71451b76c3d8f03a61582
root <root@noden36.englab.juniper.net>
2021-03-23 23:34:45 -0700

    Add n36 scripts

---
+++ 13066/n36scripts/deploy.py
@@ -0,0 +1,55 @@
+import os
+import yaml
+from subprocess import check_output
+from jinja2 import Environment, FileSystemLoader
+
+
+DEPLOY_FILE = 'my-k8s-os-mi.yaml'
+TEST_INPUT = 'test_input.yaml'
+
+
+def destroy_model():
+    os.system('juju destroy-model default --force -y')
+
+
+def add_model():
+    os.system('juju add-model default')
+    os.system('juju model-config default-space=mgmt')
+
+
+def prepare_yaml():
+    version = "R2011.L1.224" 
+    # version = input("Enter contrail version: ")
+    auth_ip = check_output("juju status|grep 5000|awk '{print $5}'", shell=True, universal_newlines=True).strip()
+
+    THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+    env = Environment(loader=FileSystemLoader(THIS_DIR))
+
+    template = env.get_template(f"{DEPLOY_FILE}.j2")
+    with open(DEPLOY_FILE, 'w') as f:
+        f.write(template.render(version=version))
+    
+    os.system(f'cp {DEPLOY_FILE} /root/CEM/13066/{DEPLOY_FILE}')
+
+    template = env.get_template(f"{TEST_INPUT}.j2")
+    with open(TEST_INPUT, 'w') as f:
+        f.write(template.render(version=version, auth_ip=auth_ip))
+
+
+def deploy_with_yaml():
+    os.system('juju deploy ./my-k8s-os-mi.yaml')
+
+
+def main():
+    destroy_model()
+    add_model()
+    prepare_yaml()
+    deploy_with_yaml()
+    os.system('nohup python3 wait_and_update.py &')
+
+if __name__ == '__main__':
+    main()
+    # prepare_yaml()
+    # os.system('nohup python3 wait_and_update.py &')
+
+
---
+++ 13066/n36scripts/etc-hosts-in-template.py
@@ -0,0 +1,87 @@
+import subprocess
+import sys
+import os
+import yaml
+
+TEST_INPUT = 'test_input.yaml'
+localhost = '127.0.1.1 {{fqdn}} {{hostname}}'
+localhosts = []
+hosts = []
+mgmt_dns = []
+ctrl_dns = []
+commands = []
+mgmt_ips = []
+ctrl_ips = []
+
+with open(TEST_INPUT) as file:
+    data = yaml.load(file, Loader=yaml.FullLoader)
+
+nodes = list(data['instances'].keys())
+for _, v in data['instances'].items():
+  mgmt_ips.append(v['ip'])
+
+# Comment control ips for single interface deployment
+for _, v in data['control_data'].items():
+  ctrl_ips.append(v['ip'])
+
+for ip in mgmt_ips:
+    os.system(f'ssh-keygen -f "/root/.ssh/known_hosts" -R "{ip}"')
+
+
+def host_mapping():
+  for i in range(len(nodes)):
+    hosts.append(f"ubuntu@{mgmt_ips[i]}")
+    # Ports are handled in ~/.ssh/config since we use OpenSSH
+    mgmt_dns.append(f"{mgmt_ips[i]} {nodes[i]}.maas {nodes[i]}")
+    if ctrl_ips[i] == "":
+      ctrl_dns.append("")
+    else:
+      ctrl_dns.append(f"{ctrl_ips[i]} {nodes[i]}.maas {nodes[i]}")
+
+  # print(hosts)
+  # print(localhosts)
+  # print(mgmt_dns)
+  # print(ctrl_dns)
+
+
+def mach_specific_sed_cmd():
+  for _ in range(len(nodes)):
+    command = f"sudo sed -i"
+    for j in range(len(nodes)):
+      command += f" -e '/{localhost}/i {mgmt_dns[j]}'"
+      if ctrl_dns[j] != "":
+        command += f" -e '/{localhost}/i {ctrl_dns[j]}'"
+    command += " /etc/cloud/templates/hosts.debian.tmpl"
+    commands.append(command)
+  # for c in commands:
+  #   print(c)
+  #   print()
+
+
+def execute_cmd_on_remote(i):
+  ssh = subprocess.Popen(["ssh", "%s" % hosts[i], commands[i]],
+                         shell=False,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+  result = ssh.stdout.readlines()
+  err = ssh.stderr.readlines()
+  print(result)
+  print(err)
+
+
+def ssh_sed(machine='all'):
+  if machine == 'all':
+    for i in range(len(nodes)):
+      execute_cmd_on_remote(i)
+  else:
+    execute_cmd_on_remote(machine)
+
+
+host_mapping()
+mach_specific_sed_cmd()
+# By default on all machines if no parameter is specified
+ssh_sed()
+# ssh_sed(machine=3)
+# ssh_sed(machine=4)
+# ssh_sed(machine=0)
+# ssh_sed(machine=2)
